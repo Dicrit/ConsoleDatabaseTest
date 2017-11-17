7e3e532712c7bf028a6272f0ced9525960b0451e
dicrit <dima.bolsunov@gmail.com>
2017-11-17 22:03:54 +0200

    initial commit

---
+++ DataBaseTest2/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+using System.Configuration;
+
+namespace DataBaseTest2
+{
+    class Program
+    {
+        static UserRepository repo = new UserRepository();
+
+
+        static void Add()
+        {
+            Console.WriteLine("\nEnter user name and age.");
+            User user = new User();
+            user.Name = Console.ReadLine();
+            user.Age = int.Parse(Console.ReadLine());
+            repo.Create(user);
+            Console.WriteLine("Success added User");
+        }
+        static void printAll()
+        {
+            Console.WriteLine("\nOurHeroes:");
+            List<User> users = repo.GetUsers();
+
+            foreach (User user in users)
+            {
+                Console.WriteLine(String.Format("{0} is {1} years old", user.Name, user.Age));
+            }
+        }
+
+        static void Delete()
+        {
+            Console.WriteLine("\nPlease, enter id of user, you'd like to delete");
+            int id = int.Parse(Console.ReadLine());
+            repo.Delete(id);
+        }
+        static void Update()
+        {
+
+
+            Console.WriteLine("\nid, name, age");
+            User user = new User();
+            user.Id = int.Parse(Console.ReadLine());
+            user.Name = Console.ReadLine();
+            user.Age = int.Parse(Console.ReadLine());
+            repo.Update(user);
+           
+        }
+
+
+        static void Main(string[] args)
+        {
+            //repo.Create(new User() { Name = "ROMAN", Age = 16 });
+            Console.WriteLine("enter character");
+            Console.WriteLine("a - Add user");
+            Console.WriteLine("p - print all users");
+            Console.WriteLine("u - update user");
+            Console.WriteLine("d - delete");
+            char c = Console.ReadKey().KeyChar;
+            switch (c)
+            {
+                case 'a':
+                    Add();
+                    break;
+                case 'u':
+                    Update();
+                    break;
+                case 'p':
+                    printAll();
+                    break;
+                case 'd':
+                    Delete();
+                    break;
+                default:
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DataBaseTest2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DataBaseTest2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DataBaseTest2")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("324c0225-e075-47ad-827b-d1cbd9592d65")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
